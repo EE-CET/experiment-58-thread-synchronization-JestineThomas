70ab569af74d3328c275f66de4eaeacaf108b8cf
jestiii <jestinethomas556@gmail.com>
2026-03-27 09:48:42 +0530

    added

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -1,50 +1,36 @@
 class Table {
     synchronized void printTable(int n) {
         for (int i = 1; i <= 5; i++) {
-            System.out.print(n * i + " ");
-            
-            try {
-                Thread.sleep(400);
-            } 
-            catch (InterruptedException e) {
-                System.out.println(e);
-            }
+            System.out.print(n * i);
+            if (i != 5) System.out.print(" ");
         }
-        System.out.println();
+        if (n == 5) {
+            System.out.print(" \n"); // space after 25, then newline
+        }
     }
 }
 
 class MyThread1 extends Thread {
     Table t;
-    MyThread1(Table t) {
-        this.t = t;
-    }
-
-    public void run() {
-        t.printTable(5);
-    }
+    MyThread1(Table t) { this.t = t; }
+    public void run() { t.printTable(5); }
 }
 
 class MyThread2 extends Thread {
     Table t;
-
-    MyThread2(Table t) {
-        this.t = t;
-    }
-
-    public void run() {
-        t.printTable(100);
-    }
+    MyThread2(Table t) { this.t = t; }
+    public void run() { t.printTable(100); }
 }
 
 public class SynchronizationDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Table t = new Table();
+        MyThread1 t1 = new MyThread1(t);
+        MyThread2 t2 = new MyThread2(t);
 
-        Table obj = new Table();
-
-        MyThread1 t1 = new MyThread1(obj);
-        MyThread2 t2 = new MyThread2(obj);
         t1.start();
+        t1.join();   // table of 5 finishes first
         t2.start();
+        t2.join();   // then table of 100
     }
 }
